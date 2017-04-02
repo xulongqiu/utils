d6a9dcaf4293dff6a4852f492cb88aeebe32dc7e
xulongqiu <xulongqiu163@163.com>
2017-04-02 11:14:57 +0800

    update shadowsockets script

--- net/shadowsocks.sh
+++ net/shadowsocks.sh
@@ -1,10 +1,9 @@
-{
-    "server":"us03-80.ssv7.net",
-    "server_port":62315,
-    "local_address":"127.0.0.1",
-    "local_port":1080,
-    "password":"FzKYNn4W3vVF",
-    "timeout":300,
-    "method":"aes-256-cfb",
-    "fast_open":false
-}
+#########################################################################
+# File Name: shadowsocks.sh
+# Author: eric.xu
+# mail: eric.xu@libratone.com.cn
+# Created Time: 2017年03月01日 星期三 00时25分43秒
+#########################################################################
+#!/bin/bash
+
+sslocal -s us03-80.ssv7.net -p 62315 -b 127.0.0.1 -l 1080 -k FzKYNn4W3vVF -m  aes-256-cfb -t 600
